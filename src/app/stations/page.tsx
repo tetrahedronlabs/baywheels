39b703bab3dbde2af35448596f10c42a12734005
Brandon <brandnlholl@gmail.com>
2025-02-13 23:08:06 -0800

    All stations on stations page

--- src/app/stations/page.tsx
+++ src/app/stations/page.tsx
@@ -1,7 +1,16 @@
-export default function StationsPage() {
+import { database } from "@/src/utils/database"
+
+export default async function StationsPage() {
+    const stationsList = await database.db.stations.select(['short_name']).getAll()
   return (
     <div>
       <h1>Stations</h1>
+      {(stationsList).map((station) => (
+        <div key={station.short_name}>
+          {station.short_name}
+        </div>
+      ))}
+
     </div>
   )
 }
