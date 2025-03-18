2a0e12a7d6f7e28dc735a25fb360f4982803fda4
Brandon <brandnlholl@gmail.com>
2025-03-17 20:36:15 -0700

    Update api

--- backend/api/src/index.ts
+++ backend/api/src/index.ts
@@ -1,4 +1,4 @@
-import { rides } from "../drizzle/schema";
+import { rides, stations } from "../drizzle/schema";
 import { drizzle } from "drizzle-orm/d1";
 import { Hono } from "hono";
 
@@ -7,6 +7,12 @@ const app = new Hono<{ Bindings: CloudflareBindings }>();
 app.get("/rides", async (c) => {
   const db = drizzle(c.env.DB);
   const result = await db.select().from(rides).all();
+  return c.json(result);
+});
+
+app.get("/stations", async (c) => {
+  const db = drizzle(c.env.DB);
+  const result = await db.select().from(stations).all();
   return c.json(result);
 });
 
