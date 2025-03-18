c2b5abf4c4aae80a765c9f84c6105c983985605a
Brandon <brandnlholl@gmail.com>
2025-03-17 21:17:08 -0700

    Get ride by id

--- backend/api/src/index.ts
+++ backend/api/src/index.ts
@@ -1,12 +1,29 @@
 import { rides, stations } from "../drizzle/schema";
 import { drizzle } from "drizzle-orm/d1";
 import { Hono } from "hono";
+import { eq } from "drizzle-orm";
 
 const app = new Hono<{ Bindings: CloudflareBindings }>();
 
 app.get("/rides", async (c) => {
   const db = drizzle(c.env.DB);
   const result = await db.select().from(rides).all();
+  return c.json(result);
+});
+
+app.get("/rides/:id", async (c) => {
+  const db = drizzle(c.env.DB);
+  const id = c.req.param("id");
+  const result = await db
+    .select()
+    .from(rides)
+    .where(eq(rides.ride_id, id))
+    .get();
+
+  if (!result) {
+    return c.json({ error: "Ride not found" }, 404);
+  }
+
   return c.json(result);
 });
 
